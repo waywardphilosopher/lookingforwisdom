392095a98b80c495c4665ffc6e7ddff9bbac058f
Will Buckingham <will@willbuckingham.com>
2022-12-30 00:08:19 +0000

    adding second test form

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -5,7 +5,7 @@ const { BUTTONDOWN_API_KEY } = process.env
 
 exports.handler = async event => {
   const payload = JSON.parse(event.body).payload
-  console.log(`Recieved a submission: ${payload.email}`)
+  console.log(`Received a submission: ${payload.email}`)
 
   return fetch("https://api.buttondown.email/v1/subscribers", {
     method: "POST",
@@ -13,11 +13,11 @@ exports.handler = async event => {
       Authorization: `Token ${BUTTONDOWN_API_KEY}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: payload.email }),
+    body: JSON.stringify({ email: payload.email, notes: payload.name }),
   })
     .then(response => response.json())
     .then(data => {
       console.log(`Submitted to Buttondown:\n ${data}`)
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
-} 
+}
